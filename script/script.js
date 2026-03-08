9b9248e75ba079c237b28b150177f0ff13954490
Sakshimathur749 <mathursakshi749@gmail.com>
2026-03-08 18:04:31 +0530

    first commit

---
+++ script/script.js
@@ -0,0 +1,20 @@
+const counters = document.querySelectorAll('.count span');
+
+counters.forEach(counter => {
+    const target = +counter.getAttribute('data-target');
+    counter.innerText = '0';
+
+    const updateCounter = () => {
+        const current = +counter.innerText;
+        const increment = Math.ceil(target / 700); // speed adjust
+
+        if(current < target){
+            counter.innerText = current + increment;
+            setTimeout(updateCounter, 10);
+        } else {
+            counter.innerText = target; // ensure exact value
+        }
+    }
+
+    updateCounter();
+});
