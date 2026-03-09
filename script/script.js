09e7b43a2f40287c049439867cf72b609b0c3e9a
Sakshimathur749 <mathursakshi749@gmail.com>
2026-03-09 14:48:55 +0530

    new changes

--- script/script.js
+++ script/script.js
@@ -1,20 +1,23 @@
-const counters = document.querySelectorAll('.count span');
+const counters = document.querySelectorAll(".count span");
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3.5,
+  spaceBetween: 20,
+});
+counters.forEach((counter) => {
+  const target = +counter.getAttribute("data-target");
+  counter.innerText = "0";
 
-counters.forEach(counter => {
-    const target = +counter.getAttribute('data-target');
-    counter.innerText = '0';
+  const updateCounter = () => {
+    const current = +counter.innerText;
+    const increment = Math.ceil(target / 700);
 
-    const updateCounter = () => {
-        const current = +counter.innerText;
-        const increment = Math.ceil(target / 700); // speed adjust
-
-        if(current < target){
-            counter.innerText = current + increment;
-            setTimeout(updateCounter, 10);
-        } else {
-            counter.innerText = target; // ensure exact value
-        }
+    if (current < target) {
+      counter.innerText = current + increment;
+      setTimeout(updateCounter, 10);
+    } else {
+      counter.innerText = target;
     }
+  };
 
-    updateCounter();
+  updateCounter();
 });
