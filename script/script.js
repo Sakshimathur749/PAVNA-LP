639ff07e0d937d0bc93e09e99d9e44333ba60569
Sakshimathur749 <mathursakshi749@gmail.com>
2026-03-11 15:10:04 +0530

    update

--- script/script.js
+++ script/script.js
@@ -1,24 +1,5 @@
 const counters = document.querySelectorAll(".count span");
 
-document.addEventListener("DOMContentLoaded", function () {
-  const observerOptions = {
-    threshold: 0.15,
-  };
-
-  const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("animate-in");
-        observer.unobserve(entry.target);
-      }
-    });
-  }, observerOptions);
-
-  document.querySelectorAll(".custom-card").forEach((card) => {
-    observer.observe(card);
-  });
-});
-
 counters.forEach((counter) => {
   const target = +counter.getAttribute("data-target");
   counter.innerText = "0";
